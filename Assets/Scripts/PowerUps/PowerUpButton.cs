429d5ea0330d7a5fee81b5ebda7ac1616f75d6a6
T-Terra <gabst138@gmail.com>
2025-09-20 22:33:22 -0300

    feat: finalizado sistema de powerup

---
+++ Assets/Scripts/PowerUps/PowerUpButton.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class PowerUpButton : MonoBehaviour
+{
+    [SerializeField] Sprite sprite;
+    [SerializeField] int level;
+    [SerializeField] TMP_Text description;
+    [SerializeField] float damage;
+    [SerializeField] int clones;
+    [SerializeField] int increaseShield;
+    [SerializeField] float coinMultiplier;
+    [SerializeField] int AbilityType;
+
+    private PowerUpCardData powerUpData;
+
+    // Chamado pelo HUD para configurar o botão
+    public void Setup(PowerUpCardData data)
+    {
+        powerUpData = data;
+
+        if (sprite != null) sprite = data.sprite;
+        level = data.level;
+        if (description != null) description.text = data.description;
+        damage = data.damage;
+        clones = data.clones;
+        increaseShield = data.increaseShield;
+        coinMultiplier = data.coinMultiplier;
+        AbilityType = data.AbilityType;
+    }
+
+    // Quando o botão é clicado
+    public void OnClick()
+    {
+        Debug.Log($"Selecionou: {powerUpData.name} (Level {powerUpData.level})");
+        // aqui você pode aplicar o efeito no jogador, ex:
+        // Player.Instance.ApplyPowerUp(powerUpData);
+    }
+}
