4802c22e7f3150b7855838bc786d8ce28ef48b1a
T-Terra <gabst138@gmail.com>
2025-09-21 23:44:53 -0300

    feat: add audio

---
+++ Assets/DontDestroy.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
